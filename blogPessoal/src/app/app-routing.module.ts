3a2ae19bd01460ecbad4d1465aa58284c3b917c9
matheus.bezerra7@hotmail.com <matheus.bezerra7@hotmail.com>
2020-10-30 17:22:58 -0300

    rota home e feed

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*criando rota , arrays*/
+  /**Abrir o site já na pagina home */
+  {path: 'vazio', redirectTo: 'home', pathMatch: 'full' },
+  /**Rota Home */
+  { path: 'home', component: HomeComponent },
+  /**Rota Feed */
+  {path: 'feed' , component: FeedComponent}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
