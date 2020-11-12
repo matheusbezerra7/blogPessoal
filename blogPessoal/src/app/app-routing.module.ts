ced8a1485fef5f799d108841a1fb1e01e394ec86
matheus.bezerra7@hotmail.com <matheus.bezerra7@hotmail.com>
2020-11-11 22:30:48 -0300

    put e delete

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+import { PutTemaComponent } from './put-tema/put-tema.component';
 
 const routes: Routes = [
   /*criando rota , arrays*/
@@ -19,7 +23,16 @@ const routes: Routes = [
   /*Rota cadastro*/
   {path: 'cadastro', component: CadastroComponent},
   /**Cadastro */
-  {path: 'cadastro-tema', component: PostTemaComponent}
+  {path: 'cadastro-tema', component: PostTemaComponent},
+  /**Editar postagem */
+  {path: 'editar-post/:id', component : PutPostagemComponent},
+  /**Deletar postagem */
+  {path: 'delete-post/:id', component: DeletePostagemComponent},
+  /**Editar tema */
+  {path: 'editar-tema/:id', component: PutTemaComponent},
+  /**Deletar Tema */
+  {path: 'delete-tema/:id', component: DeleteTemaComponent}
+
 
 ];
 
---
+++ blogPessoal/src/app/put-postagem/put-postagem.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
+
+@Component({
+  selector: 'app-put-postagem',
+  templateUrl: './put-postagem.component.html',
+  styleUrls: ['./put-postagem.component.css']
+})
+export class PutPostagemComponent implements OnInit {
+
+  postagem : Postagem = new Postagem ()
+  idPost:number
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema : number
+
+  constructor(
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+
+    window.scroll(0,0)
+
+
+    this.idPost = this.route.snapshot.params["id"]
+    this.findByIdPostagem(this.idPost)
+
+    this.findAllTemas()
+
+  }
+
+  findByIdPostagem(id: number) {
+    this.postagemService.getByIdPostagens(id).subscribe((resp: Postagem) => {
+      this.postagem = resp
+    })
+  }
+
+  salvar() {
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+    
+
+    this.postagemService.putPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      this.router.navigate(['/feed'])
+      alert('Postagem alterada com sucesso')
+    }, err => {
+      if(err.status == '500'){
+        alert('Preencha todos os campos corretamente antes de enviar')
+      }
+    })
+  }
+
+  findAllTemas(){
+    this,this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp;
+    })
+  }
+
+}
