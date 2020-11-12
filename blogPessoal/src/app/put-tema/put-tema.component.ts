33d8dfbd8b0de2990a50081a14f62b0786e488a9
matheus.bezerra7@hotmail.com <matheus.bezerra7@hotmail.com>
2020-11-11 23:01:47 -0300

    put e delete tema

--- blogPessoal/src/app/put-tema/put-tema.component.ts
+++ blogPessoal/src/app/put-tema/put-tema.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-put-tema',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./put-tema.component.css']
 })
 export class PutTemaComponent implements OnInit {
+  
+  tema: Tema = new Tema()
+  
 
-  constructor() { }
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+    
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    window.scroll(0,0)
+
+
+    let id: number = this.route.snapshot.params['id']
+    this.findByIdTema(id)
+
   }
 
+  findByIdTema(id: number){
+    this.temaService.getByIdTema(id).subscribe((resp: Tema) =>{
+      this.tema = resp;
+    })
+  }
+
+  salvar(){
+    this.temaService.putTema(this.tema).subscribe((resp: Tema) =>{
+      this.tema = resp
+      this.router.navigate(['/cadastro-tema'])
+      alert('Tema atualizado com!')
+    })
+  }
 }
